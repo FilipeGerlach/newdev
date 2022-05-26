0029df5a965fc200ce26403a1852d46363030b9b
Filipe Gerlach <filipegerlachger@gmail.com>
2022-05-26 19:11:23 -0300

    26_05

--- web/listas/main.js
+++ web/listas/main.js
@@ -1,6 +1,6 @@
 const buttonSendMessege = document.getElementById('buttonsendmessege');
 
-
+//objeto é a instancia de uma classe.
 const messages = [];
 
 function addMessage(event) {
@@ -15,13 +15,48 @@ function addMessage(event) {
     email:inputEmail.value,
     message:inputMessage.value,
   }
-
+  if(messages.length < 3){
   messages.push(message);
+  } else{
+    //chama o alert do browser
+    alert('o console esta cheio');
+  }
 
+  //pegando o formulario e utilizando um metodo do form
   document.getElementById('formu').reset();
-  console.log('...', message);
   console.log('...', messages);
 }
 
 buttonSendMessege.addEventListener('click', addMessage);
 
+
+// N veiculos formam uma classe.
+//uma forma de identificar uma classe é pela letra maiuscula no inicio.
+/*function Carro (cor, marca, modelo, ano){
+  return{
+  cor,
+  marca,
+  modelo,
+  ano
+  }
+}
+
+const carro1= Carro('vermelho', 'volks', 'gol', 2010);
+const carro2= Carro('verde', 'volks', 'fusca', 2020);
+const carro3= Carro('amarelo', 'volks', 'bola', 2021);
+const carro4= Carro('azul', 'volks', 'saveiro', 2010);
+
+
+console.log('carro ->', carro1)
+console.log('carro ->', carro2)
+console.log('carro ->', carro3)
+console.log('carro ->', carro4)
+
+function Pessoa(nome, sobrenome, idade) {
+  return{
+    nome, sobrenome, idade
+  }
+}
+const pessoa1 = Pessoa('filipe', 'gerlach', 16)
+const pessoa2 = Pessoa('erick', 'ferreita', 16)
+const pessoa3 = Pessoa('matheus', 'silva', 15)*/
