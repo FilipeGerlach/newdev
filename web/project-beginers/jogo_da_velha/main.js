99fe93c6e6ba580915b8c7346244b2f48ea28a21
Filipe Gerlach <filipegerlachger@gmail.com>
2022-06-20 21:28:01 -0300

    20-06

---
+++ web/project-beginers/jogo_da_velha/main.js
@@ -0,0 +1,31 @@
+let counter = 0;
+
+
+
+  document.getElementById('game').addEventListener('click', () => {  
+  
+    
+    if(counter % 2 == 0) {
+      event.target.innerHTML = 'X'
+      
+    }
+    else{
+      event.target.innerHTML = 'O'
+    }
+    if(document.getElementById('1').innerHTML === 'X' & document.getElementById('2').innerHTML === 'X' & document.getElementById('3').innerHTML === 'X' || document.getElementById('4').innerHTML === 'X' & document.getElementById('5').innerHTML === 'X' & document.getElementById('6').innerHTML === 'X'||  document.getElementById('7').innerHTML === 'X' & document.getElementById('8').innerHTML === 'X' & document.getElementById('9').innerHTML === 'X' ||  document.getElementById('1').innerHTML === 'X' & document.getElementById('4').innerHTML === 'X' & document.getElementById('7').innerHTML === 'X'|| document.getElementById('2').innerHTML === 'X' & document.getElementById('5').innerHTML === 'X' & document.getElementById('8').innerHTML === 'X' || document.getElementById('3').innerHTML === 'X' & document.getElementById('6').innerHTML === 'X' & document.getElementById('9').innerHTML === 'X' || document.getElementById('1').innerHTML === 'X' & document.getElementById('5').innerHTML === 'X' & document.getElementById('9').innerHTML === 'X' || document.getElementById('3').innerHTML === 'X' & document.getElementById('5').innerHTML === 'X' & document.getElementById('7').innerHTML === 'X'){
+      alert('voce ganhou xisinho')
+    }
+    if(document.getElementById('1').innerHTML === 'O' & document.getElementById('2').innerHTML === 'O' & document.getElementById('3').innerHTML === 'O' || document.getElementById('4').innerHTML === 'O' & document.getElementById('5').innerHTML === 'O' & document.getElementById('6').innerHTML === 'O'||  document.getElementById('7').innerHTML === 'O' & document.getElementById('8').innerHTML === 'O' & document.getElementById('9').innerHTML === 'O' ||  document.getElementById('1').innerHTML === 'O' & document.getElementById('4').innerHTML === 'O' & document.getElementById('7').innerHTML === 'O'|| document.getElementById('2').innerHTML === 'O' & document.getElementById('5').innerHTML === 'O' & document.getElementById('8').innerHTML === 'O' || document.getElementById('3').innerHTML === 'O' & document.getElementById('6').innerHTML === 'O' & document.getElementById('9').innerHTML === 'O' || document.getElementById('1').innerHTML === 'O' & document.getElementById('5').innerHTML === 'O' & document.getElementById('9').innerHTML === 'O' || document.getElementById('3').innerHTML === 'O' & document.getElementById('5').innerHTML === 'O' & document.getElementById('7').innerHTML === 'O'){
+      alert('voce ganhou bolinha')
+    }
+    
+    
+    counter += 1;
+  })
+
+
+  document.getElementById('reset'). addEventListener('click', () => {
+   location.reload()
+  })
+
+  
